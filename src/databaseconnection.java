63f7b22106456976703453d8f18cda9eec35ae67
mazabdul7 <48858995+mazabdul7@users.noreply.github.com>
2019-12-27 00:56:27 +0000

    Update databaseconnection.java

--- src/databaseconnection.java
+++ src/databaseconnection.java
@@ -4,12 +4,6 @@ import javax.swing.JOptionPane;
 import java.sql.Connection;
 import java.sql.SQLException;
 
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 /**
  *
  * @author mazab
